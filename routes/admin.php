cc4d331791e20af0073f337cac3ced7e74511f4c
calesj <cales.junior2002@gmail.com>
2024-11-19 10:12:31 -0300

    fix - ajustando rotas

--- routes/admin.php
+++ routes/admin.php
@@ -1,13 +1,18 @@
 <?php
 
+use App\Http\Controllers\Admin\TaskController;
 use App\Http\Controllers\Admin\UserController;
-use App\Http\Controllers\TaskController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'admin','middleware' => ['auth', 'admin']], function () {
+    /** Usuários */
     Route::get('/users', [UserController::class, 'index'])->name('admin.users.index');
+    Route::put('/users/{id}', [UserController::class, 'update'])->name('admin.users.update');
 
-    Route::get('/users/{userId}/tasks', [TaskController::class, 'tasksByUser'])->name('admin.users.tasks');
-
-    Route::put('/users/{userId}/update', [UserController::class, 'update'])->name('admin.users.update');
+    /** Tarefas */
+    Route::get('/users/{id}/tasks', [TaskController::class, 'tasksByUser'])->name('admin.users.tasks');
+    Route::post('/users/{id}/tasks', [TaskController::class, 'store'])->name('admin.tasks.store');
+    Route::put('/users/tasks/{id}', [TaskController::class, 'update'])->name('admin.tasks.update');
+    Route::delete('/users/tasks/{id}', [TaskController::class, 'destroy'])->name('admin.tasks.destroy');
 });
--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,16 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('/', [TaskController::class, 'index'])->name('dashboard');
-    Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
-    Route::put('/tasks/{id}', [TaskController::class, 'update'])->name('tasks.put');
-    Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy');
-});
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    /** Tarefas */
+    Route::get('/', [TaskController::class, 'index'])->name('dashboard');
+    Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+    Route::put('/tasks/{id}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy');
 });
 
 require __DIR__.'/auth.php';
