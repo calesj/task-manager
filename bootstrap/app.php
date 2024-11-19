e22ab5d9386da31bf9d086b35f8f8efdc5aba1c2
calesj <cales.junior2002@gmail.com>
2024-11-18 22:27:52 -0300

    feat - dando um apelido para o middleware 'admin'

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->alias([
-            Admin::class => 'admin'
+            'admin' => Admin::class
         ]);
 
         //
